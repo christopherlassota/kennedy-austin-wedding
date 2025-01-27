082608e59ccc4733411ccbbf9201b9df50293f8f
christopher lassota <christopherlassota@outlook.com>
2025-01-27 12:41:48 -0700

    create responsive story section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Story from "./Components/Story/Story";
 
 const App = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Story />
     </>
   );
 };
