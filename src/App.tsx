986088b82e2f4085a2e0ce66f47799eec8d2fcce
christopher lassota <christopherlassota@outlook.com>
2025-01-28 13:01:55 -0700

    create responsive registry component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import Itinerary from "./Components/Itinerary/Itinerary";
+import Registry from "./Components/Registry/Registry";
 import Story from "./Components/Story/Story";
 import Venue from "./Components/Venue/Venue";
 
@@ -13,6 +14,7 @@ const App = () => {
       <Story />
       <Venue />
       <Itinerary />
+      <Registry />
     </>
   );
 };
---
+++ src/Components/Registry/Registry.tsx
@@ -0,0 +1,33 @@
+import Blossom from "../../Iconography/Blossom/Blossom";
+import Dune from "../../Iconography/Dune/Dune";
+import Minty from "../../Iconography/Minty/Minty";
+import Terra from "../../Iconography/Terra/Terra";
+import "./Registry.scss";
+
+const Registry = () => {
+  return (
+    <section className="registry">
+      <h2 className="registry__title">Registry</h2>
+      <article className="registry__content">
+        <article className="registry__logos">
+          <div className="registry__logos-left">
+            <Minty />
+            <Terra />
+          </div>
+          <div className="registry__logos-right">
+            <Dune />
+            <Blossom />
+          </div>
+        </article>
+        <p className="registry__text">
+          Phasellus accumsan neque viverra ut sem aliquam purus rhoncus, morbi.
+          Ut in eget leo dui nunc. Tortor viverra magna dignissim sit. Libero eu
+          euismod risus, mauris etiam ut morbi amet in. Tortor duis dignissim
+          adipiscing sem.
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default Registry;
