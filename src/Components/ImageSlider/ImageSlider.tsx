6d837a8fbacad8c346a1ecbf2dd3990a4732031c
christopher lassota <christopherlassota@outlook.com>
2025-02-02 12:47:25 -0700

    add tracker to imageslider

--- src/Components/ImageSlider/ImageSlider.tsx
+++ src/Components/ImageSlider/ImageSlider.tsx
@@ -3,7 +3,7 @@ import image1 from "../../assets/images/sandy-millar-8vaQKYnawHw-unsplash.jpg";
 import image3 from "../../assets/images/alvaro-cvg-mW8IZdX7n8E-unsplash.jpg";
 import image4 from "../../assets/images/photos-by-lanty-O38Id_cyV4M-unsplash.jpg";
 import image5 from "../../assets/images/samantha-gades-x40Q9jrEVT0-unsplash.jpg";
-import { ChevronRight, ChevronLeft } from 'lucide-react'
+import { ChevronRight, ChevronLeft, Circle, Key } from "lucide-react";
 import { useState } from "react";
 
 const ImageSlider = () => {
@@ -38,12 +38,31 @@ const ImageSlider = () => {
           />
         ))}
       </div>
-      <button className="imageslider__button imageslider__button--left" onClick={prevImage}>
+      <button
+        className="imageslider__button imageslider__button--left"
+        onClick={prevImage}
+      >
         <ChevronLeft className="imageslider__chevron" />
       </button>
-      <button className="imageslider__button imageslider__button--right" onClick={nextImage}>
+      <button
+        className="imageslider__button imageslider__button--right"
+        onClick={nextImage}
+      >
         <ChevronRight className="imageslider__chevron" />
       </button>
+      <div className="imageslider__tracker">
+        {imagesArray.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              setImageIndex(index);
+            }}
+            className="imageslider__selection"
+          >
+            <Circle className={`imageslider__circle ${index === imageIndex ? 'imageslider__circle--current' : ''}`}/>
+          </button>
+        ))}
+      </div>
     </section>
   );
 };
