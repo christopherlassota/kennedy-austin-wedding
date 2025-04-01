b600aada3ead99adc8ba1d203b656dbd02f09d12
christopher <christopherlassota@outlook.com>
2025-04-01 02:15:40 -0600

    add jsx for dresscode

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import BottomDecoration from "./Components/BottomDecoration/BottomDecoration";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
-import Itinerary from "./Components/Itinerary/Itinerary";
 import Registry from "./Components/Registry/Registry";
 import RSVP from "./Components/RSVP/RSVP";
 import Story from "./Components/Story/Story";
-import Venue from "./Components/Venue/Venue";
 import Accommodation from "./Components/Accommodation/Accommodation";
+import DressCode from "./Components/DressCode/DressCode";
 
 const App = () => {
 
@@ -18,8 +17,7 @@ const App = () => {
       <Header />
       <Hero />
       <Story />
-      <Venue />
-      <Itinerary />
+      <DressCode />
       <Accommodation />
       <Registry />
       <RSVP />
