326070413ef7971bdc6bc7bcd9d15ad5460a3de7
christopher <christopherlassota@outlook.com>
2025-05-05 00:38:13 -0600

    change accommodations description and add link

--- src/Components/Accommodation/Accommodation.tsx
+++ src/Components/Accommodation/Accommodation.tsx
@@ -6,11 +6,12 @@ const Accommodation = () => {
       <h2 className="accommodation__title">Accommodations</h2>
       <article className="accommodation__content">
         <p className="accommodation__text">
-        If you choose to stay at the Fairmont Palliser Hotel on the night of our wedding, please use the following link to book your reservation for a reduced rate: https://book.passkey.com/go/KENNE0825. Alternatively, you may call 1-800-441-1414 and reference our group code KENNE0825 to receive a reduced rate.
+        A preferred room rate is available for our guests at the Fairmont Palliser Hotel. To reserve, please visit: https://book.passkey.com/go/KENNE0825, or call 1-800-441-1414 and reference group code KENNE0825.
         </p>
         
       </article>
-      <a href="#" className="accommodation__button-anchor">
+      <a href="https://book.passkey.com/go/KENNE0825" className="accommodation__button-anchor" target="_blank"
+            rel="noopener noreferrer">
         <button className="accommodation__button">Book a Room</button>
       </a>
     </section>
