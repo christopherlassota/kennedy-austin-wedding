6582ce0af37b3da48242cbd098d9c426f61eaf9d
christopher <christopherlassota@outlook.com>
2025-05-05 00:36:24 -0600

    correct accommodations typo

--- src/Components/Accommodation/Accommodation.tsx
+++ src/Components/Accommodation/Accommodation.tsx
@@ -3,7 +3,7 @@ import "./Accommodation.scss";
 const Accommodation = () => {
   return (
     <section className="accommodation">
-      <h2 className="accommodation__title">Accomodations</h2>
+      <h2 className="accommodation__title">Accommodations</h2>
       <article className="accommodation__content">
         <p className="accommodation__text">
         If you choose to stay at the Fairmont Palliser Hotel on the night of our wedding, please use the following link to book your reservation for a reduced rate: https://book.passkey.com/go/KENNE0825. Alternatively, you may call 1-800-441-1414 and reference our group code KENNE0825 to receive a reduced rate.
