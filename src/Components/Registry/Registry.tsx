113da325678094b58cb99f7a09f58248b38b837f
christopher <christopherlassota@outlook.com>
2025-06-12 11:08:17 -0600

    edits

--- src/Components/Registry/Registry.tsx
+++ src/Components/Registry/Registry.tsx
@@ -3,7 +3,7 @@ import "./Registry.scss";
 const Registry = () => {
   return (
     <section className="registry" id="registry">
-      <h2 className="registry__title">Registry</h2>
+      <h2 className="registry__title">Gifts</h2>
       <article className="registry__content">
         <p className="registry__text">
           Your presence at our wedding is the greatest gift we could ask for, and we are so excited to celebrate this momentous occasion with you. Should you wish to honor us with a gift, a contribution to our future together would be deeply appreciated.
